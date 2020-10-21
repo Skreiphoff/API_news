acee88e438a24b990fcb9c7392af3fa0a91c32bc
Skreiphoff <skreiphoff@gmail.com>
2020-10-21 17:28:20 +0300

    Устранена зависимость тестов от текущик данных в БД

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -26,9 +26,9 @@ class ExampleTest extends TestCase
      */
     public function testUpdateNews()
     {
-        $news = News::all()->find(9);
+        $news = News::all()->first();
         $news->preview = 'Тестовое значение';
-        $response = $this->put('api/news/9',$news->toArray());
+        $response = $this->put('api/news/'.$news->id,$news->toArray());
         $response->assertStatus(200);
     }
 
@@ -37,7 +37,7 @@ class ExampleTest extends TestCase
      */
     public function testShowNews()
     {
-        $response = $this->get('api/news/10');
+        $response = $this->get('api/news/'.News::all()->first()->id);
         $response->assertStatus(200);
     }
 
@@ -46,7 +46,10 @@ class ExampleTest extends TestCase
      */
     public function testFindByName()
     {
-        $response = $this->get('api/news/find/Quo quibusdam aut sed at. Quia dolores quis est libero.');
+        $response = News::all()->first();
+        $firstWord = $response->title;
+        $firstWord = explode(' ', $firstWord);
+        $response = $this->get('api/news/find/'.$firstWord[0]);
         $response->assertStatus(200);
     }
 
@@ -55,7 +58,7 @@ class ExampleTest extends TestCase
      */
     public function testAddDuplicateNews()
     {
-        $news = News::all()->find(9);
+        $news = News::all()->last();
         $news->id = null;
         $response = $this->post('api/news',$news->toArray());
         $response->assertStatus(422);
